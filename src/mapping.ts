10e9c72331c4b9271e12cd238892b45b491da999
Gabriel Nicolás González <gabriel@camba.coop>
2021-05-04 10:49:08 -0300

    first commit

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,91 +1,189 @@
-import { BigInt } from "@graphprotocol/graph-ts"
 import {
-  Contract,
-  ApprovalForAll,
-  MinterAdded,
-  MinterRemoved,
-  OwnershipTransferred,
-  TransferBatch,
-  TransferSingle,
-  URI,
-  WhitelistAdminAdded,
-  WhitelistAdminRemoved
-} from "../generated/Contract/Contract"
-import { ExampleEntity } from "../generated/schema"
+	ethereum,
+	BigInt,
+} from '@graphprotocol/graph-ts'
 
-export function handleApprovalForAll(event: ApprovalForAll): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
+import {
+	Account,
+	TokenRegistry,
+	Token,
+	Balance,
+	Transfer,
+} from '../generated/schema'
 
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
-  if (entity == null) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
+import {
+	ApprovalForAll as ApprovalForAllEvent,
+	MinterAdded,
+	MinterRemoved,
+	OwnershipTransferred,
+	TransferBatch  as TransferBatchEvent,
+	TransferSingle as TransferSingleEvent,
+	URI            as URIEvent,
+	WhitelistAdminAdded,
+	WhitelistAdminRemoved,
+} from '../generated/Contract/Contract'
 
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
-  }
+import { constants } from '../utils/constants'
+import { events } from '../utils/events'
+import { integers } from '../utils/integers'
+import { transactions } from '../utils/transactions'
 
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
-
-  // Entity fields can be set based on event parameters
-  entity._owner = event.params._owner
-  entity._operator = event.params._operator
-
-  // Entities can be written to the store with `.save()`
-  entity.save()
-
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
-
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract.balanceOf(...)
-  // - contract.balanceOfBatch(...)
-  // - contract.contractURI(...)
-  // - contract.create(...)
-  // - contract.creators(...)
-  // - contract.isApprovedForAll(...)
-  // - contract.isMinter(...)
-  // - contract.isOwner(...)
-  // - contract.isWhitelistAdmin(...)
-  // - contract.maxSupply(...)
-  // - contract.name(...)
-  // - contract.owner(...)
-  // - contract.supportsInterface(...)
-  // - contract.symbol(...)
-  // - contract.tokenMaxSupply(...)
-  // - contract.tokenSupply(...)
-  // - contract.totalSupply(...)
-  // - contract.uri(...)
+function fetchToken(registry: TokenRegistry, id: BigInt): Token {
+	let tokenid = registry.id.concat('-').concat(id.toHex())
+	let token = Token.load(tokenid)
+	if (token == null) {
+		token = new Token(tokenid)
+		token.registry    = registry.id
+		token.identifier  = id
+		token.totalSupply = constants.BIGINT_ZERO
+	}
+	return token as Token
 }
 
-export function handleMinterAdded(event: MinterAdded): void {}
+function fetchBalance(token: Token, account: Account): Balance {
+	let balanceid = token.id.concat('-').concat(account.id)
+	let balance = Balance.load(balanceid)
+	if (balance == null) {
+		balance = new Balance(balanceid)
+		balance.token   = token.id
+		balance.account = account.id
+		balance.value   = constants.BIGINT_ZERO
+	}
+	return balance as Balance
+}
 
-export function handleMinterRemoved(event: MinterRemoved): void {}
+function registerTransfer(
+	event:    ethereum.Event,
+	suffix:   String,
+	registry: TokenRegistry,
+	operator: Account,
+	from:     Account,
+	to:       Account,
+	id:       BigInt,
+	value:    BigInt)
+: void
+{
+	let token = fetchToken(registry, id)
+	let ev = new Transfer(events.id(event).concat(suffix.toString()))
+	ev.transaction = transactions.log(event).id
+	ev.timestamp   = event.block.timestamp
+	ev.token       = token.id
+	ev.operator    = operator.id
+	ev.from        = from.id
+	ev.to          = to.id
+	ev.value       = value
 
-export function handleOwnershipTransferred(event: OwnershipTransferred): void {}
+	if (from.id == constants.ADDRESS_ZERO) {
+		token.totalSupply = integers.increment(token.totalSupply, value)
+	} else {
+		let balance = fetchBalance(token, from)
+		balance.value = integers.decrement(balance.value, value)
+		balance.save()
+		ev.fromBalance = balance.id
+	}
 
-export function handleTransferBatch(event: TransferBatch): void {}
+	if (to.id == constants.ADDRESS_ZERO) {
+		token.totalSupply = integers.decrement(token.totalSupply, value)
+	} else {
+		let balance = fetchBalance(token, to)
+		balance.value = integers.increment(balance.value, value)
+		balance.save()
+		ev.toBalance = balance.id
+	}
 
-export function handleTransferSingle(event: TransferSingle): void {}
+	token.save()
+	ev.save()
+}
 
-export function handleURI(event: URI): void {}
+export function handleApprovalForAll(event: ApprovalForAllEvent): void {
+	// event.account
+	// event.operator
+	// event.approved
+}
 
-export function handleWhitelistAdminAdded(event: WhitelistAdminAdded): void {}
+export function handleTransferSingle(event: TransferSingleEvent): void
+{
+	let registry = new TokenRegistry(event.address.toHex())
+	let operator = new Account(event.params._operator.toHex())
+	let from     = new Account(event.params._from.toHex())
+	let to       = new Account(event.params._to.toHex())
+	registry.save()
+	operator.save()
+	from.save()
+	to.save()
 
-export function handleWhitelistAdminRemoved(
-  event: WhitelistAdminRemoved
-): void {}
+	registerTransfer(
+		event,
+		"",
+		registry,
+		operator,
+		from,
+		to,
+		event.params._id,
+		event.params._amount
+	)
+}
+
+export function handleTransferBatch(event: TransferBatchEvent): void
+{
+	let registry = new TokenRegistry(event.address.toHex())
+	let operator = new Account(event.params._operator.toHex())
+	let from     = new Account(event.params._from.toHex())
+	let to       = new Account(event.params._to.toHex())
+	registry.save()
+	operator.save()
+	from.save()
+	to.save()
+
+	let ids    = event.params._ids
+	let values = event.params._amounts
+	for (let i = 0;  i < ids.length; ++i)
+	{
+		registerTransfer(
+			event,
+			"-".concat(i.toString()),
+			registry,
+			operator,
+			from,
+			to,
+			ids[i],
+			values[i]
+		)
+	}
+}
+
+
+export function handleURI(event: URIEvent): void
+{
+	let registry = new TokenRegistry(event.address.toHex())
+	registry.save()
+
+	let token = fetchToken(registry, event.params._id)
+	token.URI = event.params._uri
+	token.save()
+}
+
+export function handleOwnershipTransferred(event: OwnershipTransferred): void
+{
+	//TODO
+}
+
+export function handleMinterAdded(event: MinterAdded): void
+{
+	//TODO
+}
+
+export function handleWhitelistAdminAdded(event: WhitelistAdminAdded): void
+{
+	//TODO
+}
+
+export function handleMinterRemoved(event: MinterRemoved): void
+{
+	//TODO
+}
+
+export function handleWhitelistAdminRemoved(event: WhitelistAdminRemoved): void
+{
+	//TODO
+}
